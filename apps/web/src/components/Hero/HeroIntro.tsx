8c70dbd218c87acc7504dab2873c3a2220546a1b
haftamudestax <haftamudestax@gmail.com>
2026-09-03 06:32:44 -0700

    Fix bugs

--- apps/web/src/components/Hero/HeroIntro.tsx
+++ apps/web/src/components/Hero/HeroIntro.tsx
@@ -1,7 +1,3 @@
 export function HeroIntro() {
-  return (
-    <div className="text-2xl text-red-600">
-      Welcome. I will implement this soon
-    </div>
-  );
+  return <div className="text-2xl text-red-600">Welcome</div>;
 }
